37984adff39718358ab3e8aac6f32edc47d801cc
SMDemas <comfysantorinitransfers@gmail.com>
2026-05-10 13:08:52 +0300

    Remove test page causing build error

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactCompiler: true,
-  // @ts-ignore
   typescript: {
     ignoreBuildErrors: true,
   },
-  // @ts-ignore
   eslint: {
     ignoreDuringBuilds: true,
   },
--- src/app/product-test/page.tsx
+++
@@ -1,6 +0,0 @@
-// src/app/product-test/page.tsx
-import ProductPage from "@/components/ProductPage/ProductPage";
-
-export default function TestPage() {
-  return <ProductPage />;
-}
